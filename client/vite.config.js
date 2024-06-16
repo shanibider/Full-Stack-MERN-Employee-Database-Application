1c16c5f87494175e794af3e6a2db105a52e70a56
Shani Bider <shanibider@gmail.com>
2024-06-16 17:07:44 +0300

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+// output the build to the backend public directory
 export default defineConfig({
   plugins: [react()],
   build: {
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
+import path from 'path';
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+// Modify server.js to Serve Frontend in Production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve('public')));
 } else {
@@ -16,6 +18,13 @@ app.use(express.json());
 // 2nd argument is the records.js file
 //"localhost:5050/record" will be handled by records.js
 app.use("/record", records);
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('public', 'index.html'));
+  });
+}
 
 // start the Express server
 app.listen(PORT, () => {
